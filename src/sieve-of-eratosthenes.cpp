4bea9efe65663982404d923f2c0042377a4ebab3
Isaac Lavender <musicman241504@gmail.com>
2024-02-02 12:41:54 +0000

    sieve optimisation i^2

--- src/sieve-of-eratosthenes.cpp
+++ src/sieve-of-eratosthenes.cpp
@@ -27,22 +27,32 @@ void printPrimes(const std::vector<bool>& nums, const size_t maxN) {
 	std::cout << '\n';
 }
 
-int main() {
+int sieve() {
 	size_t maxN {1000000000};
 	std::vector<bool> nums(maxN,true); // if true can be prime
 					   // if false definetely isn't prime
-	for (size_t p{2}; p < maxN; p++) { // iterate through 2...maxN
-		if (nums[p-2]) { // if not crossed out
-			for (size_t i{p}; i*p <=maxN; i++) {
-				nums[i*p-2] = false;
+	for (size_t i{2}; i*i < maxN; i++) { // iterate through 2...maxN
+					   // largest factor of maxN is sqrt(maxN) so p^2 < maxN
+		if (nums[i-2]) { // if not crossed out
+
+			for (size_t j{i}; i*i+i*j <=maxN; j++) { // iterates to calculate multiples of i
+								 // only need to check n > i^2 as any nums below that will already be crossed out so check for i^2 + j, i^2 + 2j, i^2 + 3j. Which is i^2 + i*j
+				nums[i*i+i*j-2] = false;
 			}
 		}
 	}
 
 	//printLoop(nums);
-	printPrimes(nums,maxN);
+	//printPrimes(nums,maxN);
+	
+
 
 
 	return 0;
 
+}
+
+int main() {
+	sieve();
+	return 0;
 }
